4e5f3ce285c9f11c67f386f3d30e450fc85c4ab2
twjin03 <twjindev@gmail.com>
2026-05-05 04:11:06 +0900

    chore: RouterProvider 설정

--- rooming/src/main.tsx
+++ rooming/src/main.tsx
@@ -1,6 +1,8 @@
-
 import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router";
 import "./styles/index.css";
-import App from "./app/App";
+import { router } from "./app/routes";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
+);
